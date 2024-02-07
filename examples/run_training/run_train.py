57d18954436c1ef0f3c687b334514ee8b1597c6f
OOLebedenko <oolebedenko@gmail.com>
2024-02-07 19:40:21 +0300

    delete unused run_dir from examples/run_training

--- examples/run_training/run_train.py
+++ examples/run_training/run_train.py
@@ -6,7 +6,6 @@ from KDNN import train
 # and fill the path_to_esm_dir in config.json according to your directory tree
 #######################################################################################################################
 
-run_dir = "example_experiment"
 config = "config.json"
 log_config = "../../KDNN/logger_config.json"
 
