6c60c293cbe29e94aceeb558d326eb4820d7689d
tregedron <polovms@gmail.com>
2024-02-12 22:02:44 +0300

    Add model with cut interactions

--- KDNN/model/model.py
+++ KDNN/model/model.py
@@ -167,3 +167,118 @@ class KdModel(BaseModel):
 
         x = global_mean_pool(x, batch)
         return self.mlp(x)
+
+
+class KdModelCut(BaseModel):
+    def __init__(self,
+                 node_feature_dim: Union[int, Tuple[int, int]],
+                 node_embedding_dim: int,
+                 edge_feature_dim: int,
+                 edge_hidden_dim: int,
+                 num_layers: int = 4,
+                 heads: int = 1,
+                 concat: bool = False,
+                 negative_slope: float = 0.2,
+                 dropout: float = 0.0,
+                 add_self_loops: bool = True,
+                 fill_value: Union[float, Tensor, str] = 0,
+                 bias: bool = True,
+                 linear_layer_nodes: bool = True,
+                 linear_layer_edges: bool = True,
+                 batchnorm: bool = True,
+                 residuals_edges: bool = True,
+                 residuals_nodes: bool = False,
+                 cutoff: float = 6.0,
+                 **kwargs,
+                 ):
+
+        super().__init__()
+        self.cutoff = cutoff
+
+        self.num_layers = num_layers
+        self.convs = ModuleList()
+        self.residuals_edges = residuals_edges
+        self.residuals_nodes = residuals_nodes
+
+        self.linear_layer_nodes = ModuleList() if linear_layer_nodes else None
+        self.linear_layer_edges = ModuleList() if linear_layer_edges else None
+        self.batchnorms = ModuleList() if batchnorm else None
+
+        self.convs.append(MetaLayer(
+            EdgeConvLayer(node_feature_dim=node_feature_dim,
+                          edge_feature_dim_in=1,
+                          edge_hidden_dim=edge_hidden_dim,
+                          edge_feature_dim_out=edge_feature_dim,
+                          residuals=self.residuals_edges),
+            GATConv(in_channels=node_feature_dim,
+                    out_channels=node_embedding_dim,
+                    concat=concat,
+                    edge_dim=edge_feature_dim,
+                    heads=heads,
+                    negative_slope=negative_slope,
+                    dropout=dropout,
+                    add_self_loops=add_self_loops,
+                    fill_value=fill_value,
+                    bias=bias,
+                    **kwargs)
+        ))
+
+        for _ in range(self.num_layers-1):
+
+            meta = MetaLayer(
+                EdgeConvLayer(node_feature_dim=node_embedding_dim,
+                              edge_feature_dim_in=edge_feature_dim,
+                              edge_hidden_dim=edge_hidden_dim,
+                              edge_feature_dim_out=edge_feature_dim,
+                              residuals=self.residuals_edges),
+                GATConv(in_channels=node_embedding_dim,
+                        out_channels=node_embedding_dim,
+                        concat=concat,
+                        edge_dim=edge_feature_dim,
+                        fill_value=0))
+
+            self.convs.append(meta)
+            if linear_layer_nodes:
+                self.linear_layer_nodes.append(nn.Linear(node_embedding_dim, node_embedding_dim))
+
+            if linear_layer_edges:
+                self.linear_layer_edges.append(nn.Linear(edge_feature_dim, edge_feature_dim))
+
+            if batchnorm:
+                self.batchnorms.append(BatchNorm(node_embedding_dim))
+
+        self.mlp = Sequential(Linear(node_embedding_dim, node_embedding_dim), ReLU(),
+                              Linear(node_embedding_dim, node_embedding_dim // 2), ReLU(),
+                              Linear(node_embedding_dim // 2, 1))
+
+    def forward(self, data):
+
+        x, edge_index, edge_attr, batch = data.x, data.edge_index, data.edge_attr, data.batch
+
+        indices = torch.nonzero(edge_attr < self.cutoff)
+        edge_attr = edge_attr[indices[:, 0]]
+        edge_index = edge_index[:, indices[:, 0]]
+
+        layers = zip(self.convs, self.batchnorms) if self.batchnorms else self.convs
+
+        for i, layer in enumerate(layers):
+            if self.batchnorms:
+                conv, batchnorm = layer
+            else:
+                conv = layer
+
+            h, edge_attr, _ = conv(x, edge_index, edge_attr=edge_attr)
+            h = F.relu(batchnorm(h)) if self.batchnorms else F.relu(h)
+
+            if self.linear_layer_nodes:
+                if i < self.num_layers - 1:
+                    h = F.relu(self.linear_layer_nodes[i](h))
+
+            if self.linear_layer_edges:
+                if i < self.num_layers - 1:
+                    edge_attr = F.relu(self.linear_layer_edges[i](edge_attr))
+
+            x = h + x if self.residuals_nodes else h
+
+        x = global_mean_pool(x, batch)
+        return self.mlp(x)
