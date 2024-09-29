1eeeb512a99f8aef721bcbdeae81e079fe992fc3
OOLebedenko <oolebedenko@gmail.com>
2024-09-29 18:44:35 +0300

    update examples (add make predictions)

--- examples/run_training/run_test.py
+++ examples/run_training/run_test.py
@@ -1,15 +1,14 @@
 from KDNN import test
 
 #######################################################################################################################
-# This examples use the EsmPretrainedModel. Please, download code from github:
-# git clone git@github.com:facebookresearch/esm.git
-# and fill the path_to_esm_dir in config.json according to your directory tree
+# This examples use the EsmPretrainedModel.
+# Please, download code from github by using script: {YOUR_PATH}/KDNN/trained_models/ESM-2/download.sh
 #######################################################################################################################
 
 config = "config.json"
 log_config = "../../KDNN/logger_config.json"
 
-for dataset_type in ["train", "test", "valid"]:
+for dataset_type in ["train", "valid", "test"]:
     test.main(config=config,
               log_config=log_config,
               dataset_type=dataset_type
--- examples/run_training/run_train.py
+++ examples/run_training/run_train.py
@@ -1,9 +1,8 @@
 from KDNN import train
 
 #######################################################################################################################
-# This examples use the EsmPretrainedModel. Please, download code from github:
-# git clone git@github.com:facebookresearch/esm.git
-# and fill the path_to_esm_dir in config.json according to your directory tree
+# This examples use the EsmPretrainedModel.
+# Please, download code from github by using script: {YOUR_PATH}/KDNN/trained_models/ESM-2/download.sh
 #######################################################################################################################
 
 config = "config.json"
